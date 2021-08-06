e4d44737b5958c5ec91f52c6cb6d479442536d50
gamestar <rinayumiho@gmail.com>
2021-08-07 01:33:44 +0800

    working on mybind

--- arguments_exercise_problems.js
+++ arguments_exercise_problems.js
@@ -1,1 +1,21 @@
 // test
+function sum1(arguments){
+    let summation = 0;
+    for(let i = 0; i < arguments.length; i ++){
+        summation += arguments[i];
+    }
+    return summation;
+}
+
+// console.log(sum1([1,2,3,4]));
+
+function sum2(...other_args){
+    let summation = 0;
+    for(let i = 0; i < other_args.length; i ++){
+        summation += other_args[i];
+    }
+    return summation;
+}
+
+console.log(sum2(1,2,3,4));
+
